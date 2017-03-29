032334416e6ce65ddc63333e9744a0da04bbf812
Tamas Lang <tamas.lang@talangsoft.org>
2017-03-29 10:58:39 +0100

    - use enum to represent maze structure variations

--- src/main/java/excelian/maze/Maze.java
+++ src/main/java/excelian/maze/Maze.java
@@ -3,6 +3,30 @@ package excelian.maze;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 
+
+enum MazeStructure {
+    WALL('X'),
+    SPACE(' '),
+    START('S'),
+    EXIT('F'),
+    NEWROW('\n');
+
+    private char charRepresentation;
+
+    MazeStructure(char charRepresentation) {
+        this.charRepresentation = charRepresentation;
+    }
+
+    public char charRepresentation() {
+        return charRepresentation;
+    }
+
+    public String representation() {
+        return Character.toString(charRepresentation);
+    }
+
+}
+
 public class Maze {
 
     private String[] mazeData;
@@ -11,19 +35,18 @@ public class Maze {
 
     private final int dimensionY;
 
-    private static final String LINEBREAK = "\n";
-
     private boolean stringContainsExactlyOneOfGivenChar(String str, char c) {
         return str.chars().filter(ch -> ch == c).count() == 1;
     }
 
     public Maze(String mazeStr) {
         Preconditions.checkArgument(!Strings.isNullOrEmpty(mazeStr), "Maze can not be empty!");
-        Preconditions.checkArgument(mazeStr.contains("S"), "Maze should have a starting point!");
-        Preconditions.checkArgument(mazeStr.contains("F"), "Maze should have an exit point!");
-        Preconditions.checkArgument(stringContainsExactlyOneOfGivenChar(mazeStr, 'S'), "Maze should have exactly one starting point!");
+        Preconditions.checkArgument(mazeStr.contains(MazeStructure.START.representation()), "Maze should have a starting point!");
+        Preconditions.checkArgument(mazeStr.contains(MazeStructure.EXIT.representation()), "Maze should have an exit point!");
+        Preconditions.checkArgument(stringContainsExactlyOneOfGivenChar(mazeStr, MazeStructure.START.charRepresentation()), "Maze should have exactly one starting point!");
 
-        this.mazeData = mazeStr.split(LINEBREAK);
+        this.mazeData = mazeStr.split(MazeStructure.NEWROW.representation());
+
         dimensionX = mazeData[0].length();
         dimensionY = mazeData.length;
     }
