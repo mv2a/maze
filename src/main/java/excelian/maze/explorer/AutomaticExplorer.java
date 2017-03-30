55e240968191ea580779a2382ded73a4476acb3c
Tamas Lang <tamas.lang@talangsoft.org>
2017-03-30 21:45:06 +0100

    - Add api comment on automatic explorer

--- src/main/java/excelian/maze/explorer/AutomaticExplorer.java
+++ src/main/java/excelian/maze/explorer/AutomaticExplorer.java
@@ -7,5 +7,10 @@ import java.util.Optional;
 
 public interface AutomaticExplorer {
 
+    /**
+     * Search the way out from the maze and returning the movement history
+     *
+     * @return the movement history that was made to find the way out or empty if no exit found
+     */
     Optional<List<MazeCoordinate>> searchWayOut();
 }
